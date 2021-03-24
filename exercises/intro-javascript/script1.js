d6b3dc9b5f7def8f40532139e5804ea3ec191a91
Nalu <aluziapantoja@gmail.com>
2021-03-24 19:16:41 -0300

    Exercicio 5

--- exercises/intro-javascript/script1.js
+++ exercises/intro-javascript/script1.js
@@ -1,20 +1,23 @@
-let a = 14;
-let b = 7;
+let a = 70;
+let b = 50;
 let soma = a + b;
 let subt = a - b;
 let mult = a * b;
 let divi = a / b;
 let modu = a % b;
-let c = 11;
+let c = 60;
 
+//ex.1
 console.log(soma, subt, mult, divi, modu);
 
+//ex.2
 if (a>b) {
     console.log("a é maior")
 } else {
     console.log("b é maior")
 }
 
+//ex. 3
 if (a>b && a>c){
     console.log("a é o maior")
 } else if (b>a && b>c) {
@@ -25,10 +28,18 @@ if (a>b && a>c){
     console.log("erro")
 }
 
+//ex. 4
 if (a>0){
     console.log("positivo")
 } else if (a<0){
     console.log("negativo")
 } else {
     console.log("zero")
+}
+
+//ex.5
+if (a+b+c == 180){
+    console.log(true)
+} else {
+    console.log(false)
 }
