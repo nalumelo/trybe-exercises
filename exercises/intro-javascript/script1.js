4a372e442e1cad6d0a99f698f24c6b3d314af5b8
Nalu <aluziapantoja@gmail.com>
2021-03-25 14:46:55 -0300

    Exercicio 9

--- exercises/intro-javascript/script1.js
+++ exercises/intro-javascript/script1.js
@@ -96,11 +96,18 @@ if (suaNota < 0 || suaNota > 100) {
 
 //ex 8
 let n1=4;
-let n2=7;
+let n2=8;
 let n3=9;
 
-if(n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0) {
+if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0) {
     console.log(true)
 } else {
+    console.log(false)
+}
+
+//ex 9
+if (n1 % 2 != 0 || n2 % 2 != 0 || n3 %2 != 0) {
+    console.log(true)
+} else{
     console.log(false)
 }
