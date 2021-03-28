d2c8b28effc0822e4447ed6c4083122f2f203a57
Nalu <aluziapantoja@gmail.com>
2021-03-28 20:04:14 -0300

    Exercicio 2

--- exercises/logica/script.js
+++ exercises/logica/script.js
@@ -7,3 +7,12 @@ for (let i = 0; i < n; i += 1) {
   }
   console.log(carac);
 }
+
+//ex 2
+for (let i = 0; i < n; i += 1) {
+    let carac ="";
+    for (let j = 0; j <= i; j += 1) {
+      carac += "*";
+    }
+    console.log(carac);
+}
