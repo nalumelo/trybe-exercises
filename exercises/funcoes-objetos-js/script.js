7acab18b68a425c01a24919d1a2d54eb644a59cd
Nalu <aluziapantoja@gmail.com>
2021-03-30 15:04:47 -0300

    Exercicio 4

--- exercises/funcoes-objetos-js/script.js
+++ exercises/funcoes-objetos-js/script.js
@@ -8,10 +8,15 @@ let info = {
 console.log('Bem-vinda, ' + info.personagem);
 
 //ex 2
-info['recorrente'] = 'sim';
+info['recorrente'] = 'Sim';
 console.log(info);
 
 //ex 3
 for (let key in info) {
   console.log(key);
+}
+
+//ex 4
+for (let key in info) {
+  console.log(info[key]);
 }
