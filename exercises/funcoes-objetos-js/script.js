e9e9a18bb7745ad5ee24d0e7647307842c953db2
Nalu <aluziapantoja@gmail.com>
2021-03-30 15:18:50 -0300

    Exercicio 5

--- exercises/funcoes-objetos-js/script.js
+++ exercises/funcoes-objetos-js/script.js
@@ -20,3 +20,19 @@ for (let key in info) {
 for (let key in info) {
   console.log(info[key]);
 }
+
+//ex 5
+let info2 = {
+ personagem: 'Tio Patinhas',
+ origem:  "Christmas on Bear Montain, Dell's Four Color Comics #178",
+ nota: 'O último MacPatinhas',
+ recorrente: 'Sim',
+}
+
+for (let key in info) {
+    if (info[key] === info.recorrente && info.recorrente === 'Sim' && info2.recorrent === 'Sim') {
+      console.log('Ambos recorrentes');
+    } else {
+      console.log(info[key] + ' e ' + info2[key]);
+    }
+}
