2e43820dc12ed4c2fe9d721f51c2237645de79df
Nalu <aluziapantoja@gmail.com>
2021-04-15 19:11:15 -0300

    Ex 6 e 7

---
+++ exercises/flexbox-responsivo-html-css/script.js
@@ -0,0 +1,11 @@
+const statesList = document.getElementById('select-estado');
+function selectState () {
+    const states = ['Selecione seu estado','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RR','RO','RJ','RN','RS','SC','SP','SE','TO'];
+    for (let index = 0; index < states.length; index += 1) {
+        const statesOp = document.createElement('option');
+        statesList.appendChild(statesOp).innerText = states[index];
+        statesList.appendChild(statesOp).value = states[index];
+    }
+}
+
+selectState();
