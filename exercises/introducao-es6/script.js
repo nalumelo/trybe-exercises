a668c5d9ab4b5f41e5706ac42c14f58d2ccca4a0
Nalu <aluziapantoja@gmail.com>
2021-04-26 18:49:25 -0300

    Ex 2

--- exercises/introducao-es6/script.js
+++ exercises/introducao-es6/script.js
@@ -11,3 +11,21 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+//ex 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const sortt = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+}
+
+const sortedArray = sortt();
+console.log(`Os números ${sortedArray} estão ordenados`);
