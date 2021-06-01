20435114645be7a265652bb00de6da2b6d7e8d66
Nalu <aluziapantoja@gmail.com>
2021-06-01 18:38:19 -0300

    Finalizada Pokedex sem estilização

--- intro-react-2/my-pokedex/src/Pokedex.js
+++ intro-react-2/my-pokedex/src/Pokedex.js
@@ -5,7 +5,7 @@ class Pokedex extends Component {
   render() {
     return (
       <section className="pokedex">
-        { this.props.pokemons.map(pokemon => <Pokemon key={pokemon, id} pokemon={pokemon} />) }
+        { this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />) }
       </section>
     )
   }
--- intro-react-2/my-pokedex/src/Pokemon.js
+++ intro-react-2/my-pokedex/src/Pokemon.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
 
-export class ; extends Component {
-    render() {
-        return (
-        )
-    }
+class Pokemon extends Component {
+  render() {
+    const { name, type, averageWeight, image } = this.props.pokemon;
+    return (
+        <div className="Pokemon">
+          <h3>{ name }</h3>
+          <h4>{ type }</h4>
+          <h4>{ `Average weight: ${ averageWeight.value } ${ averageWeight.measurementUnit }` }</h4>
+          <img src={ image } alt={ `${ name } sprite` } />
+        </div>
+    );
+  }
 }
 
-export default ;
+export default Pokemon;
